d0e81f3da6d2eb8fe72b50ea286bccdd29331991
Biswarup Ghoshal <biswarup.ghoshal@gmail.com>
2017-11-22 19:28:04 +0530

    Changed

--- debit_voucher.php
+++ debit_voucher.php
@@ -93,9 +93,6 @@ if (isset($_SESSION['user_type']) && isset($_SESSION['logged_in_user_id'])){
           </div>
           
             
-          
-        </div>
-    </div>
 
    <?php } else { echo "You are not authorized to perform any operation. Close the browser and signin again"; }
    include_once './inc/footer.php';?>
