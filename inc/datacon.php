76d41588282da62ac9239705cc3dcf13146d9bba
Biswarup <Biswarup@DESKTOP-06JDOHS>
2018-07-06 23:21:14 +0530

    Changed

--- inc/datacon.php
+++ inc/datacon.php
@@ -1,18 +1,18 @@
-<?php 
-session_start(); 
-      
-/* script to connect fo Mandir Database and pick up neccesary Information to display on screen */
-      /* declare some relevant variables */
-      $hostname = "localhost";
-      $username = "root";
-      $passwordsc = "welcome1";
-      $dbName = "kandrark_col_off_kandra";
+<?php
+session_start();
 
-      $con = mysql_connect($hostname,$username,$passwordsc);
-		if (!$con)
-		  {
-		  die('Could not connect: ' . mysql_error());
-		  }
-	mysql_select_db($dbName, $con);
+/* script to connect fo Mandir Database and pick up neccesary Information to display on screen */
+/* declare some relevant variables */
+$hostname = "localhost";
+$username = "kandrark_admin";
+$passwordsc = "welcome@123";
+$dbName = "kandrark_col_off_kandra";
+
+$con = mysql_connect($hostname,$username,$passwordsc);
+if (!$con)
+{
+    die('Could not connect: ' . mysql_error());
+}
+mysql_select_db($dbName, $con);
 
 ?>
